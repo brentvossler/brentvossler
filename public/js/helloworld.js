3939522a5b92dfc2d93656df602587583282f28c
Brent Vossler <brentvossler@icloud.com>
2018-09-19 10:37:08 -0700

    restructure

--- public/js/helloworld.js
+++ public/js/helloworld.js
@@ -1,3 +1,3 @@
-function heyThere() {
+function helloworld(){
 	document.write("Hello World");
 }
--- routes.js
+++ routes.js
@@ -5,15 +5,33 @@ module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.sendFile('index.html', {root :'./public'})
 	});
+	app.get('/resume', function(req, res) {
+		res.sendFile('resume.html', {root :'./public'})
+	});
+	app.get('/projects', function(req, res) {
+		res.sendFile('projects/projects.html', {root :'./public'})
+	});
+	app.get('/cnit132', function(req, res) {
+		res.sendFile('cnit132/cnit132.html', {root :'./public'})
+	});
+	app.get('/cnit133', function(req, res) {
+		res.sendFile('/cnit133/cnit133.html', {root :'./public'})
+	});
+		app.get('/helloworld', function(req, res) {
+		res.sendFile('CNIT133/helloworld.html', {root:'./public'})
+		});
+		app.get('/buttons', function(req, res) {
+		res.sendFile('CNIT133/buttons.html', {root:'./public'})
+		});
+		app.get('/bingo', function(req, res) {
+		res.sendFile('CNIT133/bingo.html', {root:'./public'})
+		});
+		app.get('/dice', function(req, res) {
+			res.sendFile('cnit133/dice.html', {root :'./public'})
+		});
 
-	app.get('/buttons', function(req, res) {
-		res.sendFile('/cnit133/buttons.html', {root :'./public'})
-	});
-	app.get('/HW4', function(req, res) {
-		res.sendFile('cnit133/HW4.html', {root :'./public'})
-	});
 	app.get('/cnit132hw1', function(req, res) {
-		res.sendFile('CNIT132/cnit132hw1.html', {root :'./public'})
+		res.sendFile('cnit132/cnit132hw1.html', {root :'./public'})
 	});
 	app.get('/rpg', function(req, res) {
 		res.sendFile('projects/rpg/rpg.html', {root:'./public'})
@@ -32,9 +50,6 @@ module.exports = function(app) {
 	});
 	app.get('/components', function(req, res) {
 		res.sendFile('projects/components/components.html', {root:'./public'})
-	});
-	app.get('/bingo', function(req, res) {
-		res.sendFile('CNIT133/bingo.html', {root:'./public'})
 	});
 
 	app.use(express.static("public"));
