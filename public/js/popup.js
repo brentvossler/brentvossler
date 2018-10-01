bcc4f7e987eeab41b6740f6fbe803fbe955b7cc5
Brent Vossler <brentvossler@icloud.com>
2018-10-01 15:10:56 -0700

    updated homework

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,15 +1,20 @@
-window.onload = newWinLinks;
+// function to be executed on loading
+window.onload = bv_newWinLinks;
 
-function newWinLinks() {
+// gathers all of the links on the page and makes them accessible by the loop so that if they are clicked on, the 2nd function will execute
+function bv_newWinLinks() {
 	for (var i=0; i<document.links.length; i++) {
-		if (document.links[i].className == "newWin") {
-			document.links[i].onclick = newWindow;
+		if (document.links[i].className == "bv_newWin") {
+			document.links[i].onclick = bv_newWindow;
 		}
 	}
 }
 
-function newWindow() {
-	var catWindow = window.open(this.href,"catWin","width=350,height=260");
-	catWindow.focus();
+// Whenever there is a link clicked, this function will execute. 
+function bv_newWindow() {
+	// as assigned, I also changed position of the popup by adding "left=300, top=300"
+	var bv_carWindow = window.open(this.href,"bv_carWin","width=350,height=260, left=300, top=300");
+	// This makes the new window the active window
+	bv_carWindow.focus();
 	return false;
 }
