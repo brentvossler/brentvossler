132eac142edc9a9615bca1c92383b15fd6389d6b
Brent Vossler <brentvossler@icloud.com>
2018-10-10 18:17:47 -0700

    uploaded HW

--- routes.js
+++ routes.js
@@ -2,18 +2,28 @@ var express = require('express');
 
 module.exports = function(app) {
 	
+	// Root
 	app.get('/', function(req, res) {
 		res.sendFile('index.html', {root :'./public'})
 	});
+
+	// Resume
 	app.get('/resume', function(req, res) {
-		res.sendFile('resume.html', {root :'./public'})
+		res.sendFile('/resume/resume.html', {root :'./public'})
 	});
-	app.get('/projects', function(req, res) {
-		res.sendFile('projects/projects.html', {root :'./public'})
-	});
+
+	// CNIT 132
 	app.get('/cnit132', function(req, res) {
-		res.sendFile('/CNIT132/cnit132.html', {root :'./public'})
+		res.sendFile('/cnit132/cnit132.html', {root :'./public'})
 	});
+	app.get('/cnit132hw1', function(req, res) {
+		res.sendFile('cnit132/cnit132hw1.html', {root :'./public'})
+	});
+	app.get('/cnit132hw2', function(req, res) {
+		res.sendFile('cnit132/cnit132hw2.html', {root :'./public'})
+	});
+
+	// CNIT 133
 	app.get('/cnit133', function(req, res) {
 		res.sendFile('/CNIT133/cnit133.html', {root :'./public'})
 	});
@@ -32,11 +42,16 @@ module.exports = function(app) {
 	app.get('/popup', function(req, res) {
 		res.sendFile('CNIT133/popup.html', {root :'./public'})
 	});
+	app.get('/validation', function(req, res) {
+		res.sendFile('CNIT133/validation.html', {root :'./public'})
+	});
 	app.get('/dice', function(req, res) {
 		res.sendFile('CNIT133/dice.html', {root :'./public'})
 	});
-	app.get('/cnit132hw1', function(req, res) {
-		res.sendFile('CNIT132/cnit132hw1.html', {root :'./public'})
+
+	// Projects
+	app.get('/projects', function(req, res) {
+		res.sendFile('projects/projects.html', {root :'./public'})
 	});
 	app.get('/rpg', function(req, res) {
 		res.sendFile('projects/rpg/rpg.html', {root:'./public'})
@@ -56,5 +71,6 @@ module.exports = function(app) {
 	app.get('/components', function(req, res) {
 		res.sendFile('projects/components/components.html', {root:'./public'})
 	});
+
 	app.use(express.static("public"));
 }
